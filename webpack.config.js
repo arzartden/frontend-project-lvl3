9c76043860564178e6731eadf30c22d83bcf3a3a
Arthur Arzhannikov <arzartden@yandex.ru>
2022-05-23 02:05:30 +0700

    tab size 2 for webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -14,93 +14,93 @@ const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader
 
 
 const config = {
-		entry: './src/index.js',
-		output: {
-				path: path.resolve(__dirname, 'dist'),
-		},
-		devServer: {
-				open: true,
-				host: 'localhost',
-		},
-		plugins: [
-				new HtmlWebpackPlugin({
-						template: 'index.html',
-				}),
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
 
-				// Add your plugins here
-				// Learn more about plugins from https://webpack.js.org/configuration/plugins/
-		],
-		module: {
-				rules: [
-						{
-								test: /\.(js|jsx)$/i,
-								loader: 'babel-loader',
-						},
-						{
-								test: /\.css$/i,
-								use: [
-									stylesHandler,
-									'css-loader',
-									{
-										loader: 'postcss-loader',
-										options: {
-												postcssOptions: {
-														plugins: [
-																[
-																		'autoprefixer',
-																		{
-																				// options
-																		},
-																],
-														],
-												},
-										},
-								},
-								],
-						},
-						{
-								test: /\.s[ac]ss$/i,
-								use: [
-										stylesHandler,
-										'css-loader',
-										{
-											loader: 'postcss-loader',
-											options: {
-													postcssOptions: {
-															plugins: [
-																	[
-																			'autoprefixer',
-																			{
-																					// options
-																			},
-																	],
-															],
-													},
-											},
-									},
-										'sass-loader',
-								],
-						},
-						{
-								test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-								type: 'asset',
-						},
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                  stylesHandler,
+                  'css-loader',
+                  {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: [
+                                [
+                                    'autoprefixer',
+                                    {
+                                        // options
+                                    },
+                                ],
+                            ],
+                        },
+                    },
+                },
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    stylesHandler,
+                    'css-loader',
+                    {
+                      loader: 'postcss-loader',
+                      options: {
+                          postcssOptions: {
+                              plugins: [
+                                  [
+                                      'autoprefixer',
+                                      {
+                                          // options
+                                      },
+                                  ],
+                              ],
+                          },
+                      },
+                  },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
 
-						// Add your rules for custom modules here
-						// Learn more about loaders from https://webpack.js.org/loaders/
-				],
-		},
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
 };
 
 export default () => {
-		if (isProduction) {
-				config.mode = 'production';
-				
-				config.plugins.push(new MiniCssExtractPlugin());
-				
-				
-		} else {
-				config.mode = 'development';
-		}
-		return config;
+    if (isProduction) {
+        config.mode = 'production';
+        
+        config.plugins.push(new MiniCssExtractPlugin());
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
 };
